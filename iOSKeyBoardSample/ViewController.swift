a6c19b2bae5995517e9ae0a0ba3e9a3a2b9f9aa4
GaoLianjing <xunshuairu@gmail.com>
2016-01-07 22:02:55 -0800

    no message

--- iOSKeyBoardSample/ViewController.swift
+++ iOSKeyBoardSample/ViewController.swift
@@ -16,16 +16,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let gifView = UIImageView(image: UIImage(named: "Next"))
-        gifView.frame.size = self.view.frame.size
-        self.view.addSubview(gifView)
-    
-        let url = NSURL(string: "https://49.media.tumblr.com/tumblr_md8vg1EiDu1rrx588o1_500.gif")
-        
-        gifView.sd_setImageWithURL(url)
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
